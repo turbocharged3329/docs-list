9b87eb10ba74b79a5f080ffcc44593e032e0f2be
kitaev_v <kitaev@reezon.ly>
2025-09-01 21:21:25 +0300

    Основные стили + переменные + разметка основного лейаута

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <TheHeader />
+
+  <main class="d-main-content d-box-shadow"></main>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+</script>
+
+<style scoped>
+.d-main-content {
+  width: 100%;
+  flex-grow: 1;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="d-main-header">
+    <h1 class="d-text-h1">My App</h1>
+    <p class="d-text-p1">Username</p>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.d-main-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 35px;
+  flex-shrink: 0;
+}
+</style>
